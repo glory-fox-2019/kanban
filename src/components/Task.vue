fa18bc6938e8066d93c47e7d798a2b1ec7d6a953
justarya <just1arya@gmail.com>
2019-08-21 23:12:07 +0700

    arya

---
+++ src/components/Task.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="task">
+    <div class="task--title">{{ task.name }}</div>
+    <div class="task--description">{{ task.description }}</div>
+    <div class="task--asign-to">Assign To: {{task.assign_to}}</div>
+    <div class="task--action">
+      <a href="javascript:void(0)" @click="deleteTask(task.id)">Delete</a>
+    </div>
+  </div>
+</template>
+
+<script>
+import db from '@/apis/firestore'
+
+export default {
+  props: ['task'],
+  methods: {
+    deleteTask(id){
+      db.collection("task").doc(id).delete()
+        .then(function() {
+          console.log("Document successfully deleted!");
+        }).catch(function(error) {
+          console.error("Error removing document: ", error);
+        });
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
