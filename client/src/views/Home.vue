37d52b052cf1040c820749fdbfbeaf199e2008e1
saveroz <muhammadsavero@gmail.com>
2019-08-22 13:45:32 +0700

    kanban done

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,79 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <div style="display:flex;flex-direction:row;justify-content:space-around;">
+      <TaskList :status="backlog" :taskFromParent="backlogTasks"></TaskList>
+      <TaskList :status="todo" :taskFromParent="todoTasks"></TaskList>
+      <TaskList :status="doing" :taskFromParent="doingTasks"></TaskList>
+      <TaskList :status="done" :taskFromParent="doneTasks"></TaskList>
+    </div>
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import db from '../api/firestore'
+import TaskList from '../components/TaskList'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    TaskList
+  },
+  data () {
+    return {
+      backlog: 'Back-log',
+      todo: 'Todo',
+      doing: 'Doing',
+      done: 'Done',
+      allTasks: [],
+      message: ' ahada',
+      backlogTasks: [],
+      todoTasks: [],
+      doingTasks: [],
+      doneTasks: []
+    }
+  },
+  methods: {
+    getAll () {
+      db.collection('task')
+        .onSnapshot((querySnapshot) => {
+          this.backlogTasks = []
+          this.todoTasks = []
+          this.doingTasks = []
+          this.doneTasks = []
+          querySnapshot.forEach((doc) => {
+            let obj = {
+              id: doc.id,
+              title: doc.data().title,
+              description: doc.data().description,
+              assigned: doc.data().assigned,
+              status: doc.data().status
+            }
+            if (obj.status == 0) {
+              this.backlogTasks.push(obj)
+            } else if (obj.status == 1) {
+              this.todoTasks.push(obj)
+            } else if (obj.status == 2) {
+              this.doingTasks.push(obj)
+            } else if (obj.status == 3) {
+              this.doneTasks.push(obj)
+            }
+          })
+        })
+    }
+
+  },
+  created () {
+    this.getAll()
+
+    // console.log(this.allTask)
+
+  // console.log('adad')
+    // console.log(this.allTask[0]);
+    // this.putTask()
+    // console.log(this.allTask)
   }
 }
 </script>
