decbe7d1e1eca8aa6b46c9a6f06a56a119342657
VincentiusPramudito <vpramudito@yahoo.com>
2019-08-29 18:52:25 +0700

    lastCommit

---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav class="navbar navbar-light bg-light">
+    <a class="navbar-brand">Kanzen-Kan-Ban</a>
+  </nav>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  },
+  methods: {
+    renderform () {
+      this.$emit()
+    }
+  }
+}
+</script>
+
+<style scoped>
+.navbar {
+  display: flex;
+  align-items: flex-start;
+}
+</style>
